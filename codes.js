97e5956da3d49bedc02d826f0265f33be86f7213
Newt Esmee <162417698+fallennagrand@users.noreply.github.com>
2024-03-06 09:04:59 +0200

    Create codes.js

---
+++ codes.js
@@ -0,0 +1,20 @@
+var renderClass;
+		
+		var renderers = {
+			"virtual": RendererVirtualDomHorizontal,
+			"basic": RendererBasicHorizontal,
+		};
+		
+		if(typeof this.table.options.renderHorizontal === "string"){
+			renderClass = renderers[this.table.options.renderHorizontal];
+		}else{
+			renderClass = this.table.options.renderHorizontal;
+		}
+		
+		if(renderClass){
+			this.renderer = new renderClass(this.table, this.element, this.tableElement);
+			this.renderer.initialize();
+		}else{
+			console.error("Unable to find matching renderer:", this.table.options.renderHorizontal);
+		}
+	}
